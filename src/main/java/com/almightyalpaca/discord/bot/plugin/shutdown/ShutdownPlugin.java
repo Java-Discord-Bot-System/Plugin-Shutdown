c94d6d4a044e9b8813ee992149e350ec35798e8b
Almighty-Alpaca <Almighty-Alpaca@users.noreply.github.com>
2016-04-08 23:40:53 +0200

    Updates to work with latest Core changes

--- src/main/java/com/almightyalpaca/discord/bot/plugin/shutdown/ShutdownPlugin.java
+++ src/main/java/com/almightyalpaca/discord/bot/plugin/shutdown/ShutdownPlugin.java
@@ -1,33 +1,24 @@
 package com.almightyalpaca.discord.bot.plugin.shutdown;
 
-import java.lang.reflect.Field;
-
-import com.almightyalpaca.discord.bot.system.command.AbstractCommand;
-import com.almightyalpaca.discord.bot.system.command.annotation.Command;
-import com.almightyalpaca.discord.bot.system.events.CommandEvent;
+import com.almightyalpaca.discord.bot.system.command.Command;
+import com.almightyalpaca.discord.bot.system.command.CommandHandler;
+import com.almightyalpaca.discord.bot.system.events.commands.CommandEvent;
 import com.almightyalpaca.discord.bot.system.exception.PluginLoadingException;
 import com.almightyalpaca.discord.bot.system.exception.PluginUnloadingException;
-import com.almightyalpaca.discord.bot.system.extension.PluginExtension;
 import com.almightyalpaca.discord.bot.system.plugins.Plugin;
 import com.almightyalpaca.discord.bot.system.plugins.PluginInfo;
 
 public class ShutdownPlugin extends Plugin {
 
-	class ShutdownCommand extends AbstractCommand {
+	class ShutdownCommand extends Command {
 
 		public ShutdownCommand() {
 			super("shutdown", "Shutdown the bot!", "");
 		}
 
-		@Command(dm = true, guild = true, async = true)
+		@CommandHandler(dm = true, guild = true, async = true)
 		private void onCommand(final CommandEvent event) {
-			try {
-				final Field field = ShutdownPlugin.this.getBridge().getClass().getDeclaredField("pluginExtension");
-				field.setAccessible(true);
-				((PluginExtension) field.get(ShutdownPlugin.this.getBridge())).getPluginManager().shutdown();
-			} catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
-				e.printStackTrace();
-			}
+			ShutdownPlugin.this.getBridge().getExtensionManager().shutdown();
 		}
 	}
 
